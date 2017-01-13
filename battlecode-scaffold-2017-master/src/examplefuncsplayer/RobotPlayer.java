8f99ec6665c461873f9f71531c20d6f1734972ff
Jared Frank <jfrank1120@verizon.net>
2017-01-12 21:03:55 -0500

    Changes 0.2
    Working Code that: Plants tree, spawns units, units wander around

--- battlecode-scaffold-2017-master/src/examplefuncsplayer/RobotPlayer.java
+++ battlecode-scaffold-2017-master/src/examplefuncsplayer/RobotPlayer.java
@@ -1,9 +1,12 @@
 package examplefuncsplayer;
 import battlecode.common.*;
+import java.util.*;
 
 public strictfp class RobotPlayer {
     static RobotController rc;
-    
+    static Direction[] dirList = new Direction[4];
+    static Direction goingDir;
+    static Random rand;
     MapLocation enemyBroadcastedLocations[];
     MapLocation teamBroadcastedLocations[];
     /**
@@ -15,7 +18,7 @@ public strictfp class RobotPlayer {
         // This is the RobotController object. You use it to perform actions from this robot,
         // and to get information on its current status.
         RobotPlayer.rc = rc;
-
+        initDirList();
         // Here, we've separated the controls into a different method for each RobotType.
         // You can add the missing ones or rewrite this into your own control structure.
         switch (rc.getType()) {
@@ -39,13 +42,21 @@ public strictfp class RobotPlayer {
             	break;
         }
 	}
-    static MapLocation findClosestEnemy() throws GameActionException {
-        MapLocation closestEnemy = MapLocation;
-        for (int i = 0; i < 1000; i++) {
-            rc.senseNearbyRobots()
+    public static void initDirList(){
+        for(int i=0;i<4;i++){
+            float radians = (float)(-Math.PI + 2*Math.PI*((float)i)/4);
+            dirList[i]=new Direction(radians);
+            System.out.println("made new direction "+dirList[i]);
         }
     }
-    static int treeDistance
+    static MapLocation findClosestEnemy() throws GameActionException {
+        MapLocation closestEnemy = rc.getLocation();
+        for (int i = 0; i < 1000; i++) {
+            //rc.senseNearbyRobots().
+        }
+        return closestEnemy;
+    }
+    static int treeDistance;
     static void runArchon() throws GameActionException {
         System.out.println("I'm an archon!");
         // The code you want your robot to perform every round should be in this loop
@@ -83,7 +94,7 @@ public strictfp class RobotPlayer {
         Team enemy = rc.getTeam().opponent();
         while (true) {
             try {
-
+            wander();
             } catch (Exception e) {
                 System.out.println("Tank Exception");
                 e.printStackTrace();
@@ -92,10 +103,11 @@ public strictfp class RobotPlayer {
     }
     static BulletInfo findClosestBullet (MapLocation myLocation)
     {
+        int i;
         BulletInfo[] bullets = rc.senseNearbyBullets();
         int positionInArrayOfMin = 0;
         float minDistance = bullets[0].location.distanceTo(myLocation);
-        for (int i = 1; i < bullets.length(); i++) {
+        for (i = 1; i < bullets.length; i++) {
             if (bullets[i].location.distanceTo(myLocation) < minDistance) {
                 minDistance = bullets[i].getLocation().distanceTo(myLocation);
                 positionInArrayOfMin = i;
@@ -148,7 +160,7 @@ public strictfp class RobotPlayer {
                 int xPos = rc.readBroadcast(0);
                 int yPos = rc.readBroadcast(1);
                 MapLocation archonLoc = new MapLocation(xPos,yPos);
-                
+                tryToPlant();
                 // Generate a random direction
                 Direction dir = randomDirection();
 
@@ -171,7 +183,6 @@ public strictfp class RobotPlayer {
             }
         }
     }
-
     static void runSoldier() throws GameActionException {
         System.out.println("I'm an soldier!");
         Team enemy = rc.getTeam().opponent();
@@ -207,7 +218,6 @@ public strictfp class RobotPlayer {
             }
         }
     }
-
     static void runLumberjack() throws GameActionException {
         System.out.println("I'm a lumberjack!");
         Team enemy = rc.getTeam().opponent();
@@ -250,7 +260,35 @@ public strictfp class RobotPlayer {
             }
         }
     }
-
+    public static boolean modGood(float number,float spacing, float fraction){
+        return (number%spacing)<spacing*fraction;
+    }
+    public static void tryToPlant() throws GameActionException{
+        //try to build gardeners
+        //can you build a gardener?
+        if(rc.getTeamBullets()>GameConstants.BULLET_TREE_COST) {//have enough bullets. assuming we haven't built already.
+            for (int i = 0; i < 4; i++) {
+                //only plant trees on a sub-grid
+                MapLocation p = rc.getLocation().add(dirList[i],GameConstants.GENERAL_SPAWN_OFFSET+GameConstants.BULLET_TREE_RADIUS+rc.getType().bodyRadius);
+                if(modGood(p.x,6,0.2f)&&modGood(p.y,6,0.2f)) {
+                    if (rc.canPlantTree(dirList[i])) {
+                        rc.plantTree(dirList[i]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    public static void wander() throws GameActionException {
+        try {
+            Direction dir = randomDirection();
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     /**
      * Returns a random Direction
      * @return a random Direction
